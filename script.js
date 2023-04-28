466340d69134196589adb7ec6042270a8025c187
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-28 15:12:52 -0700

    added secondary function

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 
 var searchButton = document.getElementById('search-btn');
-/*
-function getApi() {
-var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={cityname}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab';
-
-fetch(requestUrl)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-console.log(data)
-
-})
-};
-*/
 function searchState(e) {
   e.preventDefault();
 var search = document.getElementById('city-search').value.trim(); //gets city string from input
-var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q={search}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
+var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
 // var cityKeyArray = requestUrl.split(`q=`)[1];
 // var newUrl = requestUrl.replace(cityKeyArray, search + `&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`).trim();
 
@@ -28,15 +14,24 @@ fetch(requestUrl)
 .then(function (data) {
 console.log(data)
 
-
 })
 }
 
-searchButton.addEventListener('click', searchState());
+searchButton.addEventListener('click', searchState);
+//var data = 
+var lat = data.lat;
+var lon = data.lon;
 
-  /*
-var UrlString = 'http://api.openweathermap.org/geo/1.0/direct?q=SanDiego&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab';
-var stateNameAndId = UrlString.split('?q=')[1]; // makes an array from split that yeilds state+
-var Id = stateNameAndId.split('&')[1]; // splits state from Id and puts Id in an array
-var stateName = stateNameAndId.split('&')[0]; // splits state state
-*/
+function getWeather() {
+    
+var LatLonUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
+fetch(LatLonUrl)
+.then(function (response) {
+    return response.json();
+})
+.then(function (lat, lon) {
+
+console.log(lat, lon)
+
+})
+};
