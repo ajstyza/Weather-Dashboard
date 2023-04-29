f625f4cfc3ae2e31bfd8d8100b16519c4fc27e3d
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-29 15:00:22 -0700

    added display weather function to log weather conditions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 var searchButton = document.getElementById('search-btn');
 
 function searchState(e) {
@@ -16,12 +17,10 @@ var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&appi
         })
         };
 
-
- 
 function getWeather(data) {
-var lat = data.lat;
-var lon = data.lon;    
-var LatLonUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
+var lat = data[0].lat;
+var lon = data[0].lon;    
+var LatLonUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
 
 fetch(LatLonUrl)
 .then(function (response) {
@@ -30,7 +29,19 @@ fetch(LatLonUrl)
     .then(function(data) {
 
     console.log(data);
+    displayWeather(data);
 })
+};
+// function to return
+function displayWeather(data){
+    var windSpeed = data.list[0].main.wind;
+    var temperature = data.list[0].main.temp;
+    var humidity = data.list[0].main.humidity;
+    var weatherDescritpion = document.getElementById('box1').textContent;
+
+
+    console.log(windSpeed, temperature, humidity);
+    // 
 };
 
 searchButton.addEventListener('click', searchState);
