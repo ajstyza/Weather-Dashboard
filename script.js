6b10381960390196091596a9cd03c907d39bc554
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-29 13:23:48 -0700

    got correct response from first fetch again, now the rest is broken

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 
 var searchButton = document.getElementById('search-btn');
-searchButton.addEventListener('click', searchState);
-function searchState() {
+
+function searchState(e) {
+    e.preventDefault()
 var search = document.getElementById('city-search').value.trim(); //gets city string from input
-var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
+var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=5f86b2ad02bbbf3c5d9ce42a08c633ab`;
 
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
+            getWeather(data)
+        
         })
         };
-/*
+
+
+ 
 function getWeather(data) {
 var lat = data.lat;
 var lon = data.lon;    
@@ -23,9 +27,11 @@ fetch(LatLonUrl)
 .then(function (response) {
     return response.json();
 })
-.then(function (data) {
- console.log(data);
+    .then(function(data) {
+
+    console.log(data);
 })
 };
-*/
-searchState();
+
+searchButton.addEventListener('click', searchState);
+
