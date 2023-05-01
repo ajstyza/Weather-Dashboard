8007d9bd4b7947c5886a04f120625e49c7ed689e
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-05-01 16:44:12 -0700

    expanded displayWeather function

--- script.js
+++ script.js
@@ -34,15 +34,34 @@ fetch(LatLonUrl)
 };
 // function to return
 function displayWeather(data){
-    var windSpeed = data.list[0].main.wind;
+    var windSpeed = data.list[0].wind.speed;
     var temperature = data.list[0].main.temp;
     var humidity = data.list[0].main.humidity;
-    var weatherDescritpion = document.getElementById('box1').textContent;
+    document.getElementById('box1').textContent = windSpeed;
+    document.getElementById('box2').textContent = temperature;
+    document.getElementById('box3').textContent = humidity;
+
+  for (let i = 0; i < data.list.length; i+=8) {
+    const day = data.list[i];
+    var windSpeed = data.wind.speed;
+    var temperture = 
+  }
+    }
+
+    //create windEl = document.createElement("p")
+   // windEl.textContent = windSpeed;
+    // document.getElementById('box1').append(windEl);
+    // append to box1
+    
 
 
     console.log(windSpeed, temperature, humidity);
-    // 
+    
 };
 
 searchButton.addEventListener('click', searchState);
+
+
+
+//change kelvin to "imperial" to get temperture correct.
 
